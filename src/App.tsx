442aab38974850bd92c76bf42545473ba51e790b
amirgc0021 <amirgc0021@gmail.com>
2023-08-18 01:24:38 +0300

    moved theme setting to  different folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
 import { TaskList } from 'components/tasks';
 import './App.css';
-import { createTheme, ThemeProvider } from '@mui/material'
-
+import { ThemeProvider } from '@mui/material'
+import theme from "utils/theme";
 
 function App() {
 	// const [openAddTaskModal, setOpenAddTaskModal] = useState<boolean>(false);
 
-	const theme = createTheme({
-		palette: {
-			primary: {
-				main: "#0e1e2b",
-				dark: "#010305",
-				light: "#1c3d52",
-				contrastText: "#fff"
-			},
-			secondary: {
-				main: "#1c3d52",
-				light: "#F6F2F0",
-				contrastText: "#000"
-			},
 
-		},
-		spacing: [20, 42, 5],
-		typography: {
-			h1: {
-				fontSize: "37px"
-			},
-			h2: {
-				fontSize: "23px"
-			}
-		}
-	})
 
 	return (
 		<ThemeProvider theme={theme}>
---
+++ src/utils/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material'
+
+export default createTheme({
+	palette: {
+		primary: {
+			main: "#0e1e2b",
+			dark: "#010305",
+			light: "#1c3d52",
+			contrastText: "#fff"
+		},
+		secondary: {
+			main: "#1c3d52",
+			light: "#F6F2F0",
+			contrastText: "#000"
+		},
+
+	},
+	spacing: [20, 42, 5],
+	typography: {
+		h1: {
+			fontSize: "37px"
+		},
+		h2: {
+			fontSize: "23px"
+		}
+	}
+})
