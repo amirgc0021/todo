5b885264062096f9bfc3a898f0ec21c0fade8f60
amirgc0021 <amirgc0021@gmail.com>
2023-08-18 01:52:35 +0300

    added local storage function

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,29 @@
+/**
+ * Get item from local storage
+ * @param {string} key The key if the item from the storage 
+ * @param defaultVal default value to return if item doesn't exist
+ */
+export function getFromStorage<T>(key: string, defaultVal?: T): T | null {
+	const item = localStorage.getItem(key);
+
+	if (!item) return defaultVal || null;
+
+	return JSON.parse(item)
+}
+
+/**
+ * save item in local storage
+ * @param {string} key The key if the item from the storage 
+ * @param value The data itself, if data is not string data will stringify
+ */
+export function setStorage(key: string, value: any) {
+	try {
+		if (typeof value === "string")
+			localStorage.setItem(key, value)
+		else
+			localStorage.setItem(key, JSON.stringify(value))
+	}
+	catch (err) {
+		throw err
+	}
+}
